b3b59996c831e54b2e34e9aef20dfab11b24c915
Nboruff <nboruff86@gmail.com>
2021-02-14 16:02:44 -0500

    changed hello message in home.js

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h2>HELLO</h2>
+                <h2>HELLO Evan</h2>
                 <p>here is some text</p>
             </div>
         );
