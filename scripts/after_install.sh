e3a71cca9dd5fd31948a55ac2b05ba7abed21c84
Nboruff <nboruff86@gmail.com>
2021-02-14 16:49:15 -0500

    deleted scripts folder

--- scripts/after_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /var/www/html/letterboxt/
-npm install
-npm install --save react react-dom react-scripts react-particles-js
-npm install pm2 -g
--- scripts/app_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /var/www/html/letterboxt/
-npm runs start  > /dev/null 2>&1 &
