747a4c057933d04521f46bbd65069d04493d6299
Nboruff <nboruff86@gmail.com>
2021-02-16 15:10:36 -0500

    added a dropdown that doesnt do much right now

--- src/Movies.js
+++ src/Movies.js
@@ -1,16 +1,51 @@
 import React, { Component } from "react";
+import themoviedb from "./TMDB_js/themoviedb"
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
 
+themoviedb.common.api_key = "b992795e34345ff9d9aaaaf6f58b75c2"
 class Movies extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: {},
+            selectValue: '' 
+        };
+
+    }
+
+    getMovies = () => {
+        var self = this
+
+        themoviedb.genres.getMovieList({},
+            )
+    }
+
+    handleSelection = (genre) => {
+        console.log(themoviedb.genres.getMovieList(genre))
+        this.setState({
+            selectValue: genre
+        })
+    } 
+
     render() {
         return (
             <div>
                 <h2>MOVIES</h2>
                 <p>Movies can go here!</p>
-                <ol>
-                    <li>Movie 1</li>
-                    <li>Movie 2</li>
-                    <li>Movie 3</li>
-                </ol>
+                <DropdownButton 
+                    alignRight 
+                    title="Genre Dropdown" 
+                    id ="dropdown-menu-align-right"
+                    onSelect={this.handleSelection}
+                        >
+                    <Dropdown.Item eventKey="Horror">Horror</Dropdown.Item>
+                    <Dropdown.Item eventKey="Action">Action</Dropdown.Item>
+                    <Dropdown.Item eventKey="Drama">Drama</Dropdown.Item>
+                    <Dropdown.Divider />
+                </DropdownButton>
+                <h4>You selected {this.state.selectValue}</h4>
             </div>
         );
     }
