38c05e918e8e69d834d7a065b74141807c3c38d3
Nboruff <nboruff86@gmail.com>
2021-02-24 16:05:33 -0500

    updated api request to use https

--- src/TMDB_js/themoviedb.js
+++ src/TMDB_js/themoviedb.js
@@ -11,8 +11,8 @@ var theMovieDb = {};
 
 theMovieDb.common = {
   api_key: "b992795e34345ff9d9aaaaf6f58b75c2",
-  base_uri: "http://api.themoviedb.org/3/",
-  images_uri: "http://image.tmdb.org/t/p/",
+  base_uri: "https://api.themoviedb.org/3/",
+  images_uri: "https://image.tmdb.org/t/p/",
   timeout: 5000,
   language: "en-US",
   generateQuery: function(options) {
